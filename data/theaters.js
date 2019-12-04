277bb6d098005d2ce1e4d797fa78adae0cef9d72
xMilla <xime.a.milla@gmail.com>
2019-12-04 10:33:47 -0300

    completo hard

--- data/theaters.js
+++
@@ -1,9 +0,0 @@
-const fs = require ('fs');
-
-let dataTheaters = fs.readFileSync( __dirname + "\/" +'theaters.json', 'utf8');
-
-let arrayTheaters = dataTheaters.length == 0 ? [] : JSON.parse(dataTheaters);
-
-let theaters = arrayTheaters;
-
-module.exports = theaters
